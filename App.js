38a3a93aea2ec425f5c33c26eb2026fd5a381468
--global <corytyme@yahoo.com>
2020-02-25 09:49:43 -0500

    Initial commit

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App: () => React$Node = () => {
+  return (
+    <View><Text>Hi</Text></View>
+
+  );
+};
+
+export default App;
